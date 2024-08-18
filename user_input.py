a30eb5e2b00a4c94e53bdfac95d0b04c257eb19e
SolomonAsiya <solasiya@gmail.com>
2024-08-18 19:05:51 +0200

    input

---
+++ user_input.py
@@ -0,0 +1,7 @@
+name = input("Please enter your name: Solomon")
+
+age = int(input("Please enter your age: 33"))
+
+location = input("Please enter your location (city, state, country): South African")
+
+print("Hello", name, ", you are", age, "years old and live in", location)
